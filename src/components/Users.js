613ce9bb44cd4090cbcef9b8882776103d2bea8c
cyantis <cyantis@gmail.com>
2019-11-11 16:40:19 -0700

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,21 +3,25 @@ import { connect } from 'react-redux';
 
 class Users extends Component {
 
+  users(){
+    return (
+      this.props.userCount > 0
+        ? <div>
+          {this.props.users.map((user, key) => <li key={key}>{user.username}</li>)}
+          <p>Total Users: {this.props.userCount}</p>
+        </div>
+        : <div></div>
+    )
+  }
+
   render() {
 
     return (
       <div>
         <ul>
           Users!
-          {this.props.userCount > 0
-            ? this.props.users.map((user, key) => <li key={key}>{user.username}</li>)
-            : <div></div>
-          }
+          {this.users()}
         </ul>
-        {this.props.userCount > 0
-          ? <p>Total Users: {this.props.userCount}</p>
-          : <div></div>
-        }
       </div>
     )
   }
